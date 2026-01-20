fd9f6ae1ccd9a63e6d30251999b3edf5fd140483
Hyusein Osmanov <osmanovhyus@gmail.com>
2026-01-20 20:00:50 +0200

    Docker environment for sql:   - Added script for building postgres-sql image

--- docker-env-setup.sh
+++ docker-env-setup.sh
@@ -4,7 +4,7 @@ function postgres-docker-build-image() {
 
 function postgres-docker-run-container() {
     docker run                                                                 \
-        -it --name sql-practice-container                                      \
+        --name sql-practice-container                                          \
         -p 5432:5432                                                           \
         sql-practice
 }
